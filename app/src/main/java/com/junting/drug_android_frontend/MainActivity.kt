e4dd41b05f8acb809a8303e57074f1c21a2f7d7a
junting <10355098@st.thsh.tp.edu.tw>
2023-02-28 16:49:04 +0800

    style: main activity layout

--- app/src/main/java/com/junting/drug_android_frontend/MainActivity.kt
+++ app/src/main/java/com/junting/drug_android_frontend/MainActivity.kt
@@ -2,10 +2,16 @@ package com.junting.drug_android_frontend
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btn_camera_onclick(view: View) {
+        Toast.makeText(this,"測試", Toast.LENGTH_SHORT).show()
     }
 }
