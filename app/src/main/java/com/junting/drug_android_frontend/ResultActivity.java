1229c8155fe3c016d65ce92e28be2557bf7f5104
junting <10355098@st.thsh.tp.edu.tw>
2023-03-17 21:34:11 +0800

    feat: Take pictures with dynamsoft SDK

---
+++ app/src/main/java/com/junting/drug_android_frontend/ResultActivity.java
@@ -0,0 +1,26 @@
+package com.junting.drug_android_frontend;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.dynamsoft.core.CoreException;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        ImageView ivNormalize = findViewById(R.id.iv_normalize);
+
+        try {
+            ivNormalize.setImageBitmap(QuadEditActivity.mNormalizedImageResult.image.toBitmap());
+        } catch (CoreException e) {
+            e.printStackTrace();
+        }
+    }
+}
