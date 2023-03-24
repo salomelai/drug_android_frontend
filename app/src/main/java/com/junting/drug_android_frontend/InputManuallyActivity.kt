6c86f78841f9352febb4ff870a1a7900bba5b37e
junting <10355098@st.thsh.tp.edu.tw>
2023-03-24 22:42:14 +0800

    style: InputManuallyActivity

--- app/src/main/java/com/junting/drug_android_frontend/InputManuallyActivity.kt
+++ app/src/main/java/com/junting/drug_android_frontend/InputManuallyActivity.kt
@@ -1,11 +1,19 @@
 package com.junting.drug_android_frontend
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.junting.drug_android_frontend.databinding.ActivityInputManuallyBinding
 
 class InputManuallyActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityInputManuallyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_input_manually)
+        binding = ActivityInputManuallyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val frequencyOption = arrayOf(1, 2, 3, 4, 5)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, frequencyOption)
+        binding.actvFrequency.setAdapter(adapter)
     }
 }
