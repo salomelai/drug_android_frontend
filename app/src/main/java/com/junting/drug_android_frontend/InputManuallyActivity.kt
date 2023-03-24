cf334062e50211f69ffb6cb94a8314a1afae90fd
junting <10355098@st.thsh.tp.edu.tw>
2023-03-25 02:50:50 +0800

    style: InputManuallyActivity

--- app/src/main/java/com/junting/drug_android_frontend/InputManuallyActivity.kt
+++ app/src/main/java/com/junting/drug_android_frontend/InputManuallyActivity.kt
@@ -1,7 +1,10 @@
 package com.junting.drug_android_frontend
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.junting.drug_android_frontend.databinding.ActivityInputManuallyBinding
 
@@ -12,8 +15,67 @@ class InputManuallyActivity : AppCompatActivity() {
         binding = ActivityInputManuallyBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initOndemandCheckbox()
+        initFrequencyDropdown()
+        initTimingsCheckbox()
+        initDosageDropdown()
+        initButton()
+
+    }
+
+    private fun initButton() {
+        binding.btnCancel.setOnClickListener {
+            super.onBackPressed()
+        }
+    }
+
+    private fun initDosageDropdown() {
+        val dosageOption = arrayOf(1, 2, 3, 4, 5)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, dosageOption)
+        binding.actvDosage.setAdapter(adapter)
+
+
+        binding.actvDosage.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            Toast.makeText(this, "您選擇了：$selectedItem", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun initOndemandCheckbox() {
+        binding.cbOnDemand.setOnCheckedChangeListener { _, isChecked ->
+            binding.tilFrequency.visibility = if (isChecked) View.GONE else View.VISIBLE
+            binding.timingsOuterContainer.visibility = if (isChecked) View.GONE else View.VISIBLE
+        }
+
+    }
+
+    private fun initTimingsCheckbox() {
+        binding.cbBeforeMeal.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.cbAfterMeal.isEnabled = !isChecked
+            binding.cbWithFood.isEnabled = !isChecked
+        }
+
+        binding.cbAfterMeal.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.cbBeforeMeal.isEnabled = !isChecked
+            binding.cbWithFood.isEnabled = !isChecked
+        }
+
+        binding.cbWithFood.setOnCheckedChangeListener { buttonView, isChecked ->
+            binding.cbBeforeMeal.isEnabled = !isChecked
+            binding.cbAfterMeal.isEnabled = !isChecked
+        }
+    }
+
+    private fun initFrequencyDropdown() {
         val frequencyOption = arrayOf(1, 2, 3, 4, 5)
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, frequencyOption)
         binding.actvFrequency.setAdapter(adapter)
+
+
+        binding.actvFrequency.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
+            val selectedItem = parent.getItemAtPosition(position).toString()
+            Toast.makeText(this, "您選擇了：$selectedItem", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
