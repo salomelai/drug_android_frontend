b7200fcdca824694778ee1c500b0966e40880610
楊宗翰 <feveraly@gmail.com>
2023-04-24 21:07:33 +0800

    feat: update today reminder list

--- app/src/main/java/com/junting/drug_android_frontend/ui/todayReminder/TodayReminderViewAdapter2.kt
+++ app/src/main/java/com/junting/drug_android_frontend/ui/todayReminder/TodayReminderViewAdapter2.kt
@@ -1,23 +1,25 @@
 package com.junting.drug_android_frontend.ui.todayReminder
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.recyclerview.widget.RecyclerView
 import com.ernestoyaquello.dragdropswiperecyclerview.DragDropSwipeAdapter
-import com.junting.drug_android_frontend.R
 import com.junting.drug_android_frontend.databinding.RemindItemViewBinding
 import com.junting.drug_android_frontend.model.today_reminder.TodayReminder
+import java.util.Collections
 
-class TodayReminderViewAdapter2(dataSet: List<String> = emptyList())
-    : DragDropSwipeAdapter<String, TodayReminderViewAdapter2.ViewHolder>(dataSet) {
+class TodayReminderViewAdapter2(val viewModel: TodayReminderViewModel)
+    :DragDropSwipeAdapter<TodayReminder, TodayReminderViewAdapter2.ViewHolder>(Collections.emptyList()) {
 
     class ViewHolder(private val binding: RemindItemViewBinding) : DragDropSwipeAdapter.ViewHolder(binding.root) {
         val itemText: TextView = binding.tvDrugName
         val dragIcon: ImageView = binding.dragIcon
+    }
+
+    fun update(todayReminder: List<TodayReminder>) {
+        dataSet = todayReminder
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodayReminderViewAdapter2.ViewHolder {
@@ -26,15 +28,20 @@ class TodayReminderViewAdapter2(dataSet: List<String> = emptyList())
         return ViewHolder(binding)
     }
 
-    override fun onBindViewHolder(item: String, viewHolder: TodayReminderViewAdapter2.ViewHolder, position: Int) {
+    override fun onBindViewHolder(item: TodayReminder, viewHolder: TodayReminderViewAdapter2.ViewHolder, position: Int) {
         // Here we update the contents of the view holder's views to reflect the item's data
-        viewHolder.itemText.text = item
+        viewHolder.itemText.text = item.drug.name
     }
-    override fun getViewHolder(itemView: View): ViewHolder {
+
+    override fun getViewHolder(itemLayout: View): ViewHolder {
         TODO("Not yet implemented")
     }
 
-    override fun getViewToTouchToStartDraggingItem(item: String, viewHolder: TodayReminderViewAdapter2.ViewHolder, position: Int): View? {
+    override fun getItemCount(): Int {
+        return viewModel.records.value?.size ?: 0
+    }
+
+    override fun getViewToTouchToStartDraggingItem(item: TodayReminder, viewHolder: TodayReminderViewAdapter2.ViewHolder, position: Int): View? {
         // We return the view holder's view on which the user has to touch to drag the item
         return viewHolder.dragIcon
     }
