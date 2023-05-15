4708845010e079e6bba0a9678e57ef492025261a
junting <10355098@st.thsh.tp.edu.tw>
2023-05-16 02:01:30 +0800

    style: drug reminder page

---
+++ app/src/main/java/com/junting/drug_android_frontend/DrugReminderActivity.kt
@@ -0,0 +1,11 @@
+package com.junting.drug_android_frontend
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DrugReminderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drug_reminder)
+    }
+}
