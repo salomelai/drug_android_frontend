fe1f30b0d0c0ee098de8a45dec033564c614bdff
si-yu Lai <coffee30456@gmail.com>
2023-07-02 09:22:37 +0800

    change google api position

--- app/src/main/java/com/junting/drug_android_frontend/WelcomePageActivity.kt
+++ app/src/main/java/com/junting/drug_android_frontend/WelcomePageActivity.kt
@@ -80,7 +80,7 @@ class WelcomePageActivity : AppCompatActivity() {
             .setGoogleIdTokenRequestOptions(
                 BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
                     .setSupported(true) // Your server's client ID, not your Android client ID.
-                    .setServerClientId(getString(R.string.web_client_id)) // Show all accounts on the device.
+                    .setServerClientId(BuildConfig.Web_Client_Id) // Show all accounts on the device.
                     .setFilterByAuthorizedAccounts(false)
                     .build()
             )
